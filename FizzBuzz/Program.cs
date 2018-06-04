f513ac995b7819a894564fd7f107a6c59597cae1
WKSefton <william.k.sefton@gmail.com>
2018-06-04 09:58:01 -0400

    Initial Commmit

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -7,6 +7,11 @@ using System.Threading.Tasks;
 namespace FizzBuzz {
 	class Program {
 		static void Main(string[] args) {
+			for (int i = 1; i <= 100; i++) {
+
+				Console.WriteLine((i % 15 == 0) ? "Fizz Buzz" : (i % 3 == 0) ? "Fizz" : (i % 5 == 0) ? "Buzz" : $"{i}");
+
+			}
 		}
 	}
 }
